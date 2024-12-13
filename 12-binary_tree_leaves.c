b3dee4c52f0c20e7806113671553691523a145b4
Bree-Coding <brendapollagba@gmail.com>
2024-12-13 23:19:23 +0100

    tried to fix task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,6 +10,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t right = 0;
 	size_t left = 0;
+	size_t sum = 0;
 
 	if (tree == NULL)
 	{
@@ -18,15 +19,11 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	left = binary_tree_leaves(tree->left);
 	right = binary_tree_leaves(tree->right);
+	sum = left + right;
 
-	if (left > right)
+	if (right == 0 && left == 0)
 	{
-		return (left + 1);
+		return (sum + 1);
 	}
-	else
-	{
-		return (left + 1);
-	}
-
-	return (0);
+	return (sum);
 }
