6965ffced31796ae2ff26c8aac4568b64a400aa4
Bree-Coding <brendapollagba@gmail.com>
2024-12-13 12:43:42 +0100

    tried to fix task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,22 +11,23 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right = 0;
 	size_t left = 0;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+	
+	if (tree->left)
+	{
+	left = 1 + binary_tree_height(tree->left);
+	}
+	if (tree->right)
+	{
+	right = 1 + binary_tree_height(tree->right);
+	}
 
 	if (left > right)
 	{
-		return (left + 1);
+		return (left);
 	}
-	else
-	{
-		return (left + 1);
-	}
-
-	return (0);
+	return (right);
 }
